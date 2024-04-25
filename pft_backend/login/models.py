04c307855f6d6e7538ee917419a24a5b756ce344
Sergio Garzón <sergio.garzon.mar@gmail.com>
2024-04-24 20:10:14 -0500

    Setting features about connection with PostgreSQL database on render

--- pft_backend/login/models.py
+++ pft_backend/login/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# Creating model for userdata table.
+class UserData(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_email = models.CharField(max_length=50, blank=False)
+    user_name = models.CharField(max_length=50, blank=False)
+    user_last_name = models.CharField(max_length=50, blank=False)
+    user_occupation_name = models.CharField(max_length=80, blank=False)
+    user_location_name = models.CharField(max_length=150, blank=False)
+    signup_date = models.CharField(max_length=10, blank=False)  # yyyy-mm-dd
+    record_date = models.DateTimeField(auto_now_add=True)
+    last_update_date = models.CharField(max_length=50, blank=True, default="P")
