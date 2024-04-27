74aa04f73aa5fc0d885a2caa22deadc4529b5266
Sergio Garzón <sergio.garzon.mar@gmail.com>
2024-04-27 18:37:37 -0500

    View app login was created

--- pft_backend/login/urls.py
+++ pft_backend/login/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from .views import *
 
-url_patterns = [
+urlpatterns = [
     # paths for all user data
     re_path(f'^signup/?$', user_data_api),
     re_path(f'^signup/([0-9]+)/?$', user_data_api),
