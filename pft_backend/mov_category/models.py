36299ead9a5bd55dd86a2c62e0c6e5907df9374d
Sergio Garzon Mariño <sergio.garzon.mar@gmail.com>
2024-10-12 17:19:33 -0500

    fixing typos in url path

--- pft_backend/mov_category/models.py
+++ pft_backend/mov_category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Creating models for movement category table.
 class MovCategory(models.Model):
     mov_catg_id = models.AutoField(primary_key=True)
-    main_category = models.CharField(max_length=30, blank=False)
+    main_category = models.CharField(max_length=30, blank=False) #personal_finances or trips
     category_name = models.CharField(max_length=30, blank=False)
     category_description = models.CharField(max_length=80, blank=True)
     record_date = models.DateTimeField(auto_now_add=True)
