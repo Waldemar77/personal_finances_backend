56f9a2c764cc72c33e2e340123f2b2d1902ad47e
Sergio Garzon Mariño <sergio.garzon.mar@gmail.com>
2024-10-12 12:24:18 -0500

    Creating new endpoint to consult all mov_category records

--- pft_backend/mov_category/urls.py
+++ pft_backend/mov_category/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    # >>> paths for all user data
+    # >>> path for all movement categories
+    re_path(f'^all_mov_catg/?$', views.all_mov_category_api),
+
+    # >>> paths for working with id mov_category
     re_path(f'^mov_catg/?$', views.mov_category_api),
     re_path(f'^mov_catg/([0-9]+)/?$', views.mov_category_api)
 ]
