c404d247373107c5353e98acb0e8b3f45f73add9
Sergio Garzon Mariño <sergio.garzon.mar@gmail.com>
2024-10-17 21:07:12 -0500

    Adding new function to saving budget

--- pft_backend/budget_data/urls.py
+++ pft_backend/budget_data/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     # endpoint to get all budget data by user_id
     re_path(f'^all_budget_user/([0-9]+)/?$', views.budget_by_user_api ),
     # endpoint to get and save budget data by user and period
-    re_path(f'^budget_user_period/([0-9]+)/([0-9_-]+)/?$', views.bugdet_by_user_period)
+    re_path(f'^budget_user_period/([0-9]+)/([0-9_-]+)/?$', views.budget_by_user_period),
+    # endpoint to saving new budget:
+    re_path(f'^saving_budget/?$', views.saving_budget),
 ]
